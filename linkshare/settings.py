4afba473b70d206378896a75c4c5e2d381f38aa5
VinCoD <5895.2020@students.ku.ac.ke>
2022-08-05 23:35:24 +0300

    added base templates to template settings

--- linkshare/settings.py
+++ linkshare/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'linkshare.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
