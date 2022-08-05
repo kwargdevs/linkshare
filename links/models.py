5662bbf8260dba3673850961cbde91506f5f521c
VinCoD <5895.2020@students.ku.ac.ke>
2022-08-05 23:32:08 +0300

    Added Link model to admin

--- links/models.py
+++ links/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Link(models.Model):
+    title = models.CharField(max_length=200)
+    link = models.URLField(max_length=500)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
