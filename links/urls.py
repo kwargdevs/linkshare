dd126404719cfc4fe4534606b58ccf1150e63a1c
VinCoD <5895.2020@students.ku.ac.ke>
2022-08-19 06:05:19 +0100

    Added frontend

--- links/urls.py
+++ links/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'links'
 
 urlpatterns = [
-    
+    path('', views.links, name='links'),
 ]
--- links/views.py
+++ links/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Link
 # Create your views here.
+
+def links(request):
+    links = Link.objects.all()
+    context = {'links': links }
+    return render(request, 'links/links.html', context)
