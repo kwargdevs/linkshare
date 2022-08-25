ec2033c4139ac900bccbb210f73556dccfde4d2d
VinCoD <5895.2020@students.ku.ac.ke>
2022-08-25 19:16:40 +0300

    Added new model Category -  Every link belongs to a category

--- links/models.py
+++ links/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    date_created = models.DateTimeField(auto_now_add=True)
+
 
 class Link(models.Model):
     title = models.CharField(max_length=200)
