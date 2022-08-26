0770c14d867bfd8f8d18cdc2c72090c880e814f7
VinCoD <5895.2020@students.ku.ac.ke>
2022-08-26 18:44:41 +0300

    Deleted the database file

--- links/models.py
+++ links/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Category(models.Model):
     name = models.CharField(max_length=200)
     date_created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Link(models.Model):
-    title = models.CharField(max_length=200)
-    link = models.URLField(max_length=500)
+    name = models.CharField(max_length=100)
+    url = models.URLField(max_length=500)
     date_added = models.DateTimeField(auto_now_add=True)
+    # author = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title
+        return self.name
