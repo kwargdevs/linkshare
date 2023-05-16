37327bd226ef71ab96b8006048ed2d06649ea585
VinCoD <5895.2020@students.ku.ac.ke>
2023-05-16 11:36:01 +0300

    Created view for homepage

--- links/views.py
+++ links/views.py
@@ -1,23 +1,25 @@
 from django.shortcuts import render
 from .models import Link
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, TemplateView
 from django.urls import reverse_lazy
 # Create your views here.
 
-# def links(request):
-#     links = Link.objects.all()
-#     context = {'links': links }
-#     return render(request, 'links/links.html', context)
+
+class IndexTemplateView(TemplateView):
+    template_name = "links/index.html"
 
 class LinksListView(ListView):
     model = Link
-    template_name = "links.html"
+    template_name = "links/links.html"
+
 
 class LinksDetailView(DetailView):
     model = Link
-    template_name = "links_detail.html"
+    template_name = "links/links_detail.html"
+
+
 class LinksCreateView(CreateView):
     model = Link
-    template_name = "link_update.html"
+    template_name = "links/link_update.html"
     fields = ["name", "url", "category"]
     success_url = reverse_lazy("links")
