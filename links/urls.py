dad7f2b9bc2efd560dd05c35df6b9c88fa53edce
VinCoD <5895.2020@students.ku.ac.ke>
2023-05-16 11:35:37 +0300

    Created url for homepage

--- links/urls.py
+++ links/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import LinksListView, LinksCreateView, LinksDetailView
+from .views import LinksListView, LinksCreateView, LinksDetailView, IndexTemplateView
 
 app_name = "links"
 
 urlpatterns = [
+    path("", IndexTemplateView.as_view(), name="index"),
     path("links/create/", LinksCreateView.as_view(), name="link_create"),
     path("links/<int:pk>", LinksDetailView.as_view(), name="link_detail"),
     path("links", LinksListView.as_view(), name="links"),
