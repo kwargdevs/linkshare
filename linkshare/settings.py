0bf1af6a8dc9a4fc21ee7a4ea4079c27675e5b63
VinCoD <5895.2020@students.ku.ac.ke>
2023-06-02 14:51:47 +0300

    Added static configurations for deployment

--- linkshare/settings.py
+++ linkshare/settings.py
@@ -121,6 +121,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles" # new
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- linkshare/urls.py
+++ linkshare/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns # new
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("links.urls")),
 ]
+
+urlpatterns += staticfiles_urlpatterns() # new
