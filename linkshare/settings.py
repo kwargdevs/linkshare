bb558f2b1ea7453f82c20d6015b0c816e1df4af5
VinCoD <5895.2020@students.ku.ac.ke>
2023-06-02 14:55:47 +0300

    Added static configurations for deployment

--- linkshare/settings.py
+++ linkshare/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-3e^3&10jh#xu89br!)!ig37o9)d_5w5v5+36g2844!$gvdbrum
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
