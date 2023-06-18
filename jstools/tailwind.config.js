872b15a0fe20666e928843085e291316b90cd52f
VinCoD <5895.2020@students.ku.ac.ke>
2023-06-18 09:18:08 +0300

    Added tailwind command to tailwind.config.js then ran build

--- jstools/tailwind.config.js
+++ jstools/tailwind.config.js
@@ -1,19 +1,59 @@
 module.exports = {
-    future: {
-        removeDeprecatedGapUtilities: true,
-        purgeLayersByDefault: true,
-    },
-    content: {
-        enabled: false, //true for production build
-        content: [
-            '../**/templates/*.html',
-            '../**/templates/**/*.html'
-        ]
-    },
-    theme: {
-        extend: {},
-    },
-    variants: {},
-    plugins: [],
+  darkMode: 'class',
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: {
+    enabled: false, //true for production build
+    content: [
+      '../**/templates/*.html',
+      '../**/templates/**/*.html'
+    ]
+  },
+  theme: {
+    extend: {
+      colors: {
+        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a"}
+      }
+      },
+    fontFamily: {
+      'body': [
+        'Inter',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'system-ui',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'
+      ],
+      'sans': [
+        'Inter',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'system-ui',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'
+      ]
+    }
+  },
+  variants: {},
+  plugins: [],
 }
-
