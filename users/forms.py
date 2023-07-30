bac0f92fe6e33b2d55e8163a9b9b841782535508
codewitgabi <gabrielmicheal497@gmail.com>
2023-07-30 22:20:32 +0100

    Added styling for password reset view

--- users/forms.py
+++ users/forms.py
@@ -115,7 +115,8 @@ class ResetPasswordForm(PasswordResetForm):
             "name": "email",
             "type": "email",
             "id": "email",
-            "placeholder": "Email"
+            "placeholder": "Email",
+            "class": "bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         })
     )
     
