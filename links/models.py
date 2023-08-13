9fbdaa9c8acd5deb2da5f9e4c09478d26dee34e1
codewitgabi <gabrielmicheal497@gmail.com>
2023-08-13 05:59:23 +0100

    Completed link creation view.

--- links/models.py
+++ links/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
-
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
     date_created = models.DateTimeField(auto_now_add=True)
+    
     class Meta:
         verbose_name_plural = "Categories"
 
@@ -19,10 +18,10 @@ class Link(models.Model):
     url = models.URLField(max_length=500)
     date_added = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(Category, default=None, on_delete=models.CASCADE)
-
+    description = models.TextField(default="")
+    
+    def get_absolute_url(self):
+        return reverse("links:link_detail", kwargs={"pk": self.pk})
 
     def __str__(self):
         return self.name
-    
-    
-
