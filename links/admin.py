8dd06d575924ae78607e88b15f18d1119f9d1ed5
codewitgabi <gabrielmicheal497@gmail.com>
2023-08-14 22:44:44 +0100

    Updated links models to follow all features of the project.

--- links/admin.py
+++ links/admin.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
+from .models import Link, Tag, LinkComment
 
-from .models import Link, Category
 
-# Register your models here.
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    """
+    Link model admin descriptor
+    """
+    
+    list_display = ("tag", "url","submitter", "date_added")
+    list_filter = ("tag", "votes", "date_added")
+    search_fields = ("tag", "url", "date_added")
 
-admin.site.register(Link)
-admin.site.register(Category)
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    """
+    Tag model admin descriptor
+    """
+    
+    list_display = ("name", "date_created")
+    list_filter = ("name", "date_created")
+    search_fields = ("name",)
+    
+
+@admin.register(LinkComment)
+class LinkCommentAdmin(admin.ModelAdmin):
+    """
+    LinkComment model admin descriptor
+    """
+    
+    list_display = ("link", "comment", "date_created")
+    list_filter = ("link", "date_created")
--- links/models.py
+++ links/models.py
@@ -1,27 +1,60 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+# User object
+
+User = get_user_model()
 
 
-class Category(models.Model):
+class Tag(models.Model):
     name = models.CharField(max_length=200)
     date_created = models.DateTimeField(auto_now_add=True)
     
     class Meta:
-        verbose_name_plural = "Categories"
+        indexes = [
+            models.Index(fields=["name"], name="name_index")
+        ]
 
     def __str__(self):
         return self.name
 
 
 class Link(models.Model):
-    name = models.CharField(max_length=100)
-    url = models.URLField(max_length=500)
-    date_added = models.DateTimeField(auto_now_add=True)
-    category = models.ForeignKey(Category, default=None, on_delete=models.CASCADE)
+    """
+    For upvote and downvote, use thw following code snippet
+    >>> instance.votes.add(user) # single upvote
+    >>> instance.votes.add(user1, user2, user3) # multiple upvotes
+    
+    >>> instance.votes.remove(user) # single downvote
+    >>> instance.votes.add(user1, user2, user3) # multiple downvotes
+    """
+    
+    title = models.CharField(max_length=100)
+    url = models.URLField(max_length=500)   
+    tag = models.ForeignKey(Tag, default=None, on_delete=models.CASCADE)
+    submitter = models.ForeignKey(User, on_delete=models.CASCADE, related_name="uploader")
     description = models.TextField(default="")
+    votes = models.ManyToManyField(User)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=["title", "url", "tag"], name="main_idxs"),
+            models.Index(fields=["description"], name="sub_idxs")
+        ]
     
     def get_absolute_url(self):
         return reverse("links:link_detail", kwargs={"pk": self.pk})
 
     def __str__(self):
         return self.name
+
+
+class LinkComment(models.Model):
+    link = models.ForeignKey(Link, on_delete=models.CASCADE)
+    comment = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.comment
